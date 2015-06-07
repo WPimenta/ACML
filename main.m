a7de7de97ceb4757ea3d4e490578671aba1239db
David Kroukamp <tipteq@gmail.com>
2015-06-07 15:36:30 +0200

    Got the algorithm to work.. but not recursively

--- main.m
+++ main.m
@@ -30,6 +30,14 @@ for optionNum=1:size(options)
 end
 disp('=====================================');
 
+count = 1;
+for optionNum=1:size(options)
+    if (optionNum ~= maxInfGainOption)
+        remainingOptions{count} = optionNum;
+        count = count + 1;
+    end
+end
+
 disp('Check for definite yes or no');
 for option=1:size(options{maxInfGainOption}, 2)
     [yes, no] = GetNumYesNo(all_data, options{maxInfGainOption}{option}, maxInfGainOption, all_results);
@@ -49,7 +57,19 @@ for option=1:size(options{maxInfGainOption}, 2)
         PPositive = numPositive / size(specificResults,1);
         PNegative = numNegative / size(specificResults,1);
         
-        HS = Mult(-PPositive,log2(PPositive)) -Mult(PNegative,log2(PNegative));        
+        HS = Mult(-PPositive,log2(PPositive)) -Mult(PNegative,log2(PNegative));
+        
+        disp('Information Gains');
+        maxInformationGain2 = 0;
+        maxInfGainOption2 = 0;
+        for optionNum=1:size(remainingOptions,2)
+            infGain = InfGain(specificRecords, specificResults, options, remainingOptions{optionNum}, HS);
+            disp([headers{remainingOptions{optionNum}} ' = ' num2str(infGain)]);
+            if (infGain > maxInformationGain2)
+                maxInformationGain2 = infGain;
+                maxInfGainOption2 = optionNum;
+            end
+        end
     end
 end
 disp('=====================================');
