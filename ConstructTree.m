e6b69e97cdc66cf60609c6dd33dd8f6e121279a8
WPimenta <WPimenta@users.noreply.github.com>
2015-06-07 16:33:11 +0200

    Create ConstructTree.m

---
+++ ConstructTree.m
@@ -0,0 +1,22 @@
+function [ node ] = ConstructTree( S )
+%CONSTRUCTTREE Summary of this function goes here
+%   Detailed explanation goes here
+
+    numNeg = countNeg;
+    numPos = CountPos;
+    if(numNeg == 0 || numPos == 0)
+        if(numNeg > 0)
+            node = 'no';
+        elseif(numPos > 0)
+            node = 'yes';
+        end
+    else
+        nodeIndex = MaxInfGainNode(S);
+        node = S{nodeIndex};
+        for i = 1:sizeof(S{nodeIndex})
+            S_ = newS(S);
+            ConstructTree(S_);
+        end
+    end
+end
+
